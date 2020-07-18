0f8afc65dbc21ea02c9d16fde6b1549c5293136c
unknown <ernedoselami@hotmail.com>
2020-07-18 11:37:21 +0200

    changes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,14 @@
+class IdValidator < ActiveModel::Validator
+    def validate(record)
+       if options[:fields].any?{|field| record.send(field) == @iddd }
+       	      record.errors[:base] << "This value is incorrect"
+    end
+    end
+end
 class Task < ApplicationRecord
+	  include ActiveModel::Validations
+
 	validates :name, :lname ,presence: true
+    validates_with IdValidator, fields: [:idd]
+
 end
